4527ff2e2d481b75f73c1419d8470d5dbe65f9fd
Softcial <scerdasb@ucenfotec.ac.cr>
2026-06-16 16:02:21 -0600

    feat(auth): wire platform auth dependencies

--- app-frontend/shared/src/androidMain/kotlin/io/github/themonstersp4/mejengueros/di/modules/PlatformModule.android.kt
+++ app-frontend/shared/src/androidMain/kotlin/io/github/themonstersp4/mejengueros/di/modules/PlatformModule.android.kt
@@ -1,5 +1,9 @@
 package io.github.themonstersp4.mejengueros.di.modules
 
+import io.github.themonstersp4.mejengueros.data.auth.AndroidOAuthBrowser
+import io.github.themonstersp4.mejengueros.data.auth.IOAuthBrowser
+import io.github.themonstersp4.mejengueros.data.auth.IRandomStringGenerator
+import io.github.themonstersp4.mejengueros.data.auth.SecureRandomStringGenerator
 import io.github.themonstersp4.mejengueros.data.local.DriverFactory
 import io.github.themonstersp4.mejengueros.data.remote.HttpClientFactory
 import org.koin.android.ext.koin.androidContext
@@ -9,4 +13,6 @@ import org.koin.dsl.module
 actual fun platformModule(): Module = module {
   single { DriverFactory(androidContext()) }
   single { HttpClientFactory() }
+  single<IRandomStringGenerator> { SecureRandomStringGenerator() }
+  single<IOAuthBrowser> { AndroidOAuthBrowser(androidContext()) }
 }
--- app-frontend/shared/src/iosMain/kotlin/io/github/themonstersp4/mejengueros/di/modules/PlatformModule.ios.kt
+++ app-frontend/shared/src/iosMain/kotlin/io/github/themonstersp4/mejengueros/di/modules/PlatformModule.ios.kt
@@ -1,5 +1,9 @@
 package io.github.themonstersp4.mejengueros.di.modules
 
+import io.github.themonstersp4.mejengueros.data.auth.IOAuthBrowser
+import io.github.themonstersp4.mejengueros.data.auth.IRandomStringGenerator
+import io.github.themonstersp4.mejengueros.data.auth.IosOAuthBrowser
+import io.github.themonstersp4.mejengueros.data.auth.SecureRandomStringGenerator
 import io.github.themonstersp4.mejengueros.data.local.DriverFactory
 import io.github.themonstersp4.mejengueros.data.remote.HttpClientFactory
 import org.koin.core.module.Module
@@ -8,4 +12,6 @@ import org.koin.dsl.module
 actual fun platformModule(): Module = module {
   single { DriverFactory() }
   single { HttpClientFactory() }
+  single<IRandomStringGenerator> { SecureRandomStringGenerator() }
+  single<IOAuthBrowser> { IosOAuthBrowser() }
 }
