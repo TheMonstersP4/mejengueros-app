b62c3a4a02c6784f75676295f8050529fd1c5cd6
Softcial <scerdasb@ucenfotec.ac.cr>
2026-06-16 17:46:34 -0600

    feat(auth): persist iOS auth in keychain

--- app-frontend/shared/src/iosMain/kotlin/io/github/themonstersp4/mejengueros/di/modules/PlatformModule.ios.kt
+++ app-frontend/shared/src/iosMain/kotlin/io/github/themonstersp4/mejengueros/di/modules/PlatformModule.ios.kt
@@ -1,7 +1,9 @@
 package io.github.themonstersp4.mejengueros.di.modules
 
+import io.github.themonstersp4.mejengueros.data.auth.IAuthSecureStorage
 import io.github.themonstersp4.mejengueros.data.auth.IOAuthBrowser
 import io.github.themonstersp4.mejengueros.data.auth.IRandomStringGenerator
+import io.github.themonstersp4.mejengueros.data.auth.IosAuthSecureStorage
 import io.github.themonstersp4.mejengueros.data.auth.IosOAuthBrowser
 import io.github.themonstersp4.mejengueros.data.auth.SecureRandomStringGenerator
 import io.github.themonstersp4.mejengueros.data.local.DriverFactory
@@ -12,6 +14,7 @@ import org.koin.dsl.module
 actual fun platformModule(): Module = module {
   single { DriverFactory() }
   single { HttpClientFactory() }
+  single<IAuthSecureStorage> { IosAuthSecureStorage(get()) }
   single<IRandomStringGenerator> { SecureRandomStringGenerator() }
   single<IOAuthBrowser> { IosOAuthBrowser() }
 }
